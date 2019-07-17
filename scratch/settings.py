85bce5758081c18250443c6a28fb3146c4edff27
ClaudiaP <claudia.prd23@gmail.com>
2019-07-17 11:56:49 +0300

    Create base.html and add DataTable refs #13

--- scratch/settings.py
+++ scratch/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'scratch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
