002469273b1bd36218c35cd21ed02b1092a95770
Andrei-Ştefan TRIFU (87919) <andrei_stefan.trifu@stud.acs.upb.ro>
2019-07-19 17:34:43 +0300

    Login and Logout added refs #15

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TendersListView
+from .views import TendersListView, LoginView
 
 urlpatterns = [
     path('tenders/', TendersListView.as_view(), name='tenders_list_view'),
+    path('login/', LoginView.as_view(), name='login_view'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,56 @@
 from django.views.generic.list import ListView
 from .models import Tender
+from django.utils.http import is_safe_url
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import FormView, RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TendersListView(ListView):
+class TendersListView(LoginRequiredMixin, ListView):
     model = Tender
     context_object_name = 'tenders'
     template_name = 'tenders_list.html'
+    login_url = '/app/login'
+    redirect_field_name = 'login_view'
+
+
+class LoginView(FormView):
+    success_url = '/app/tenders'
+    form_class = AuthenticationForm
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'login.html'
+
+    @method_decorator(sensitive_post_parameters('password'))
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        request.session.set_test_cookie()
+
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+
+        return super(LoginView, self).form_valid(form)
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get(self.redirect_field_name)
+        if not is_safe_url(url=redirect_to, allowed_hosts=self.request.get_host()):
+            redirect_to = self.success_url
+        return redirect_to
+
+
+class LogoutView(RedirectView):
+    url = '/app/login'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
