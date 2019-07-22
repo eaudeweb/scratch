3ce580d354707e8a16e5e9a1ee3ea376f46908ab
Andrei-Ştefan TRIFU (87919) <andrei_stefan.trifu@stud.acs.upb.ro>
2019-07-22 12:31:25 +0300

    Add logout button and functionality refs #16

--- scratch/settings.py
+++ scratch/settings.py
@@ -66,6 +66,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries': {
+                'tags': 'app.tags.tags',
+            }
         },
     },
 ]
