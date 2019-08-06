7668fadf20bb50d33203fabfc2bf98b6691eab6b
ClaudiaP <claudia.prd23@gmail.com>
2019-08-06 17:52:13 +0300

    Add TenderDocumnets test cases

--- app/tests/test_add_tenders.py
+++ app/tests/test_add_tenders.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from app.models import Tender
+from app.models import Tender, TenderDocument
 
 
 class AddTenderTestCase(TestCase):
     def setUp(self):
         Tender.objects.create(reference='RFC/TEST/123', url='http://test.com', source='UNGM', unspsc_codes='98765')
-        Tender.objects.create(reference='RFC/TEST/1234', url='http://test.org', source='TED', unspsc_codes='12345')
+        ted_tender = Tender.objects.create(reference='RFC/TEST/1234', url='http://test.org', source='TED',
+                                           unspsc_codes='12345')
+        TenderDocument.objects.create(tender=ted_tender, download_url='http://test.org/1', name='doc_1')
+        TenderDocument.objects.create(tender=ted_tender, download_url='http://test.org/2', name='doc_2')
 
     def test_add_tenders(self):
         t1 = Tender.objects.get(reference='RFC/TEST/123')
@@ -19,3 +22,27 @@ class AddTenderTestCase(TestCase):
         tenders = Tender.objects.all()
         self.assertEqual(len(tenders), 1)
         self.assertEqual(tenders[0].reference, 'RFC/TEST/1234')
+
+    def test_add_tender_documents(self):
+        tender = Tender.objects.get(reference='RFC/TEST/1234')
+        documents_list = TenderDocument.objects.filter(tender=tender)
+        self.assertEqual(len(documents_list), 2)
+
+        doc_1 = TenderDocument.objects.get(tender=tender, name='doc_1')
+        doc_2 = TenderDocument.objects.get(name='doc_2')
+
+        self.assertEqual(doc_1.download_url, 'http://test.org/1')
+        self.assertEqual(doc_2.download_url, 'http://test.org/2')
+
+    def test_delete_tender_document(self):
+        doc = TenderDocument.objects.get(name='doc_1')
+        doc.delete()
+        documents = TenderDocument.objects.filter(tender=doc.tender)
+        self.assertEqual(len(documents), 1)
+        self.assertEqual(documents.first().name, 'doc_2')
+
+    def test_delete_tender_and_documents(self):
+        tender = Tender.objects.get(reference='RFC/TEST/1234')
+        tender.delete()
+        documents = TenderDocument.objects.filter(tender=tender)
+        self.assertEqual(len(documents), 0)
