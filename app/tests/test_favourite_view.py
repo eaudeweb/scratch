09542fdd8fbda1b06ec7675be3a6df2ea646063d
ClaudiaP <claudia.prd23@gmail.com>
2019-08-07 15:56:47 +0300

    Create tests for TenderFavouriteView

---
+++ app/tests/test_favourite_view.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from app.models import Tender
+from django.contrib.auth.models import User
+
+
+def create_tender(status):
+    return Tender.objects.create(
+        title='Tender3',
+        reference='RFC/TEST/1237',
+        url='http://test.com',
+        source='UNGM',
+        unspsc_codes='98765',
+        favourite=True,
+    )
+
+
+def get_url(id):
+    return '/app/tenders/favourite/' + str(id) + '/'
+
+
+class TendersFavouriteTests(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='test_user')
+        user.set_password('12345')
+        user.save()
+        logged_in = self.client.login(username='test_user', password='12345')
+        self.assertEqual(logged_in, True)
+
+        new_tender = create_tender(False)
+        url = get_url(new_tender.id)
+        response = self.client.post(url, {'favourite': 'true'})
+        self.assertEqual(response.status_code, 200)
+        tender_added = Tender.objects.get(reference='RFC/TEST/1237')
+        self.assertEqual(tender_added.favourite, True)
+        tender_added.delete()
+
+    def test_remove_from_favourites(self):
+        new_tender = create_tender(True)
+        url = get_url(new_tender.id)
+        response = self.client.post(url, {'favourite': 'false'})
+        self.assertEqual(response.status_code, 200)
+        tender_added = Tender.objects.get(reference='RFC/TEST/1237')
+        self.assertEqual(tender_added.favourite, False)
+        tender_added.delete()
---
+++ app/tests/test_tenders_list_view.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from app.models import Tender, TenderDocument
+from django.contrib.auth.models import User
+
+
+def create_tenders():
+    Tender.objects.create(
+        title='Tender1',
+        reference='RFC/TEST/12345',
+        url='http://test.com',
+        source='UNGM',
+        unspsc_codes='1234'
+    )
+    ted_tender = Tender.objects.create(
+        title='Tender2',
+        reference='RFC/TEST/123456',
+        url='http://test.org',
+        source='TED',
+        unspsc_codes='12345'
+    )
+    TenderDocument.objects.create(tender=ted_tender, download_url='http://test.org/1', name='doc_1')
+    TenderDocument.objects.create(tender=ted_tender, download_url='http://test.org/2', name='doc_2')
+
+
+class TendersListViewTests(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='test_user')
+        user.set_password('12345')
+        user.save()
+        logged_in = self.client.login(username='test_user', password='12345')
+        self.assertEqual(logged_in, True)
+
+    def test_list_view_empty_db(self):
+        response = self.client.get('/app/tenders/')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['tenders'], [])
+
+    def test_list_view_two_tenders(self):
+        create_tenders()
+        response = self.client.get('/app/tenders/')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            list(response.context['tenders']),
+            ['<Tender: Tender1>', '<Tender: Tender2>']
+        )
+        self.assertEqual(response.context['tenders'][0].reference, 'RFC/TEST/12345')
+        self.assertEqual(response.context['tenders'][1].source, 'TED')
