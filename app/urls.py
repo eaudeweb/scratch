76ff7c4dd23ee4cb2322fd5964ac4eea48723583
thorion21 <blazegamingro@gmail.com>
2019-08-13 15:58:32 +0300

    Homepage, Overview, bug-fixing refs #75 refs #76 refs #77 refs #89

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
 
 from .views import LoginView, LogoutView, ContractAwardsListView
-from .views import TendersListView, TenderDeleteView, TenderDetailView, TenderFavouriteView
+from .views import (
+    TendersListView,
+    TenderDeleteView,
+    TenderDetailView,
+    TenderFavouriteView,
+    HomepageView,
+    OverviewPageView,
+)
 
 
 urlpatterns = [
-    path('tenders/', TendersListView.as_view(), name='tenders_list_view'),
-    path('tenders/<int:pk>/', TenderDetailView.as_view(), name='tender_detail_view'),
-    path('tenders/favourite/<int:pk>/', TenderFavouriteView.as_view(), name='tender_favourite_view'),
-    path('tenders/delete/<int:pk>/', TenderDeleteView.as_view(), name='tender_delete_view'),
-    path('login/', LoginView.as_view(), name='login_view'),
-    path('logout/', LogoutView.as_view(), name='logout_view'),
-    path('awards/', ContractAwardsListView.as_view(), name='contract_awards_list_view'),
+    path("", HomepageView.as_view(), name="homepage_view"),
+    path("tenders/", TendersListView.as_view(), name="tenders_list_view"),
+    path("tenders/<int:pk>/", TenderDetailView.as_view(), name="tender_detail_view"),
+    path("tenders/favourite/<int:pk>/", TenderFavouriteView.as_view(), name="tender_favourite_view"),
+    path("tenders/delete/<int:pk>/", TenderDeleteView.as_view(), name="tender_delete_view"),
+    path("overview/", OverviewPageView.as_view(), name="overview_view"),
+    path("login/", LoginView.as_view(), name="login_view"),
+    path("logout/", LogoutView.as_view(), name="logout_view"),
+    path("awards/", ContractAwardsListView.as_view(), name="contract_awards_list_view"),
 ]
