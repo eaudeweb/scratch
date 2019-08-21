ea8232df93d021bd25bb64426576299b225f17c7
ClaudiaP <claudia.prd23@gmail.com>
2019-08-21 17:08:26 +0300

    Install Elasticsearch

--- app/documents.py
+++ app/documents.py
@@ -1,4 +1,17 @@
-from django_elasticsearch_dsl import Document
-from django_elasticsearch_dsl.registries import registry
-from .models import Tender, TenderDocument, Winner, UNSPSCCode, CPVCode
+from django_elasticsearch_dsl import  DocType, Index
+from .models import Tender, Winner, UNSPSCCode, CPVCode
 
+tender = Index('tenders')
+tender.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
+
+@tender.doc_type
+class WinnerDocument(DocType):
+    class Meta:
+        model = Tender
+        fields = [
+            'title',
+        ]
