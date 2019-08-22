58a6244e6948de56a5ba9d094bf73ff1f9f674c6
ClaudiaP <claudia.prd23@gmail.com>
2019-08-22 14:08:22 +0300

    Index Tenders and Winner model refs #93

--- app/documents.py
+++ app/documents.py
@@ -1,4 +1,4 @@
-from django_elasticsearch_dsl import  DocType, Index
+from django_elasticsearch_dsl import  DocType, Index, fields
 from .models import Tender, Winner, UNSPSCCode, CPVCode
 
 tender = Index('tenders')
@@ -7,11 +7,36 @@ tender.settings(
     number_of_replicas=0
 )
 
+winner = Index('winners')
+winner.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
 
 @tender.doc_type
-class WinnerDocument(DocType):
+class TenderDocument(DocType):
     class Meta:
         model = Tender
         fields = [
             'title',
+            'reference',
+            'description',
+            'unspsc_codes',
+            'cpv_codes',
+            'organization',
+            'source',
+        ]
+
+
+@winner.doc_type
+class WinnerDocument(DocType):
+    tender_title = fields.KeywordField(attr='tender.title')
+
+    class Meta:
+        model = Winner
+        fields = [
+            'vendor',
+            'value',
+            'currency',
         ]
