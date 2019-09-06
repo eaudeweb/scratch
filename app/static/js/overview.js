6055a6acaf71a6b5d11b610f54ac85ba19b68dd0
ClaudiaP <claudia.prd23@gmail.com>
2019-09-06 16:35:29 +0300

    Paginate latest scrapping sessions refs #140

---
+++ app/static/js/overview.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  let scroll_size = $('#log_container').height() * 0.8
+  $('#log_table').DataTable({
+    "order": [[ 0, "desc" ]],
+    "lengthChange": false,
+    "searching": false,
+    "pageLength": 6,
+    "bInfo": false,
+    "columnDefs": [
+      { "orderable": false, "targets": 0 }
+    ],
+    "dom": 't<"bottom"flp><"clear">',
+    "bScrollCollapse" : false,
+    "sScrollY": scroll_size,
+  });
+});
