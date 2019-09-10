42d304ff6788fc929084f8bc6ce9f7bd673414ee
ClaudiaP <claudia.prd23@gmail.com>
2019-09-10 16:37:11 +0300

    Update delete tenders command refs #143

--- app/management/commands/delete_expired_tenders.py
+++ app/management/commands/delete_expired_tenders.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         for tender in tenders:
             time_diff = tender.deadline - current_time
             days = time_diff.days
-            if int(days) < 0 and abs(days) >= settings.DELETE_EXPIRED_DAYS:
+            if days < 0 and abs(days) >= int(settings.DELETE_EXPIRED_DAYS):
                 tender.delete()
 
         return self.stdout.write(self.style.SUCCESS('Tenders older than %d days deleted'
