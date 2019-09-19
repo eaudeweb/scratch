083fea61401548b2bc8fe5d03e7acb67a0634424
Alex Neamtu <alexneamtu@gmail.com>
2019-09-19 17:53:18 +0300

    Import TED tenders for the last 5 years #163

--- app/management/commands/update_ted_5years.py
+++ app/management/commands/update_ted_5years.py
@@ -1,6 +1,10 @@
 import logging
-from datetime import datetime, date
-from app.parsers.ted import TEDWorker
+from datetime import datetime, date, timedelta
+
+from django.utils.timezone import make_aware
+from django_q.tasks import async_task
+
+from app.models import Task
 from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
 
@@ -18,23 +22,36 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-
-        given_date = None
         now = date.today()
-        default = now - relativedelta(years=5)
+        default = now - relativedelta(days=5)
 
         try:
             given_date = datetime.strptime(options['date'], '%d/%m/%Y').date()
         except TypeError:
+            given_date = default
             logger.warning('No date was provived or it was provided but does not match the format (e.g. 19/05/2017)')
             logger.warning('Default value of 5 years ago is chosen')
 
         try:
-            w = TEDWorker(given_date or default)
-            w.ftp_download_latest_archives()
-            w.parse_notices()
+            today = date.today()
+
+            command_name = "update_ted_5years"
+            command = 'app.parsers.ted.process_daily_archive'
+
+            while given_date < today:
+                task_id = async_task(command, given_date)
+
+                t = Task(
+                    id=task_id,
+                    args=command_name,
+                    kwargs=f'given_date: {given_date}',
+                    started=make_aware(datetime.now()),
+                )
+                t.save()
+
+                given_date = given_date + timedelta(days=1)
+
         except Exception as e:
             logging.critical(e)
 
-        self.stdout.write(self.style.SUCCESS(f"TED tenders updated (since {options['date'] or default})"))
-        return f"TED tenders updated (since {options['date'] or default})"
+        return self.stdout.write(self.style.SUCCESS(f"Queued TED tenders updates (since {options['date'] or default})"))
