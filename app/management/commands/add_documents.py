70e0add8380da74d553360d5d617df8fb1bf9a0e
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2022-10-18 19:35:10 +0300

    Add command that downloads missing documents

---
+++ app/management/commands/add_documents.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from app.models import TenderDocument
+from app.parsers.ungm import UNGMWorker
+
+
+class Command(BaseCommand):
+    help = "Adds documents where missing for each TenderDocument object"
+
+    def handle(self, *args, **options):
+
+        tender_docs = TenderDocument.objects.all()
+
+        for doc in tender_docs:
+            if not doc.document:
+                self.stdout.write(self.style.SUCCESS("Downloading document %s" % doc.name))
+                UNGMWorker.download_document(doc)
+
