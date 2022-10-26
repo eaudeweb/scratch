7b03369102eca3ba8593dab56161c4b171a55cc2
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2022-10-26 18:58:16 +0300

    Add django-sql-explorer

--- scratch/urls.py
+++ scratch/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('explorer/', include('explorer.urls')),
     path('', include('app.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
