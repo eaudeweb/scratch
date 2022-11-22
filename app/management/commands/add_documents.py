4618914314b2db544c6472b375b26f2e433b8b49
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2022-11-22 10:15:01 +0200

    Add add_documents fix to migrations

--- app/management/commands/add_documents.py
+++ app/management/commands/add_documents.py
@@ -14,5 +14,8 @@ class Command(BaseCommand):
         for doc in tender_docs:
             if not doc.document:
                 self.stdout.write(self.style.SUCCESS("Downloading document %s" % doc.name))
-                UNGMWorker.download_document(doc)
+                try:
+                    UNGMWorker.download_document(doc)
+                except Exception as e:
+                    self.stdout.write(e)
 
