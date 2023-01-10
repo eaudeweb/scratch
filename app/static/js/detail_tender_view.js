f02f356a53f30489ab873086c3e9589294d734ce
dlgauss <d.lopatenco@yandex.ru>
2023-01-10 14:29:08 +0000

    detail_tender_view.js fixed

--- app/static/js/detail_tender_view.js
+++ app/static/js/detail_tender_view.js
@@ -1,14 +1,16 @@
 
 function pushNewTagToTable(tagInputValue) {
+    tagInputValue = tagInputValue.trim()
+
     const tagsList = $("#tags_list_displayed")
     const tagsListValues = tagsList.text().split(',')
 
-    const trimmedArray = tagsListValues.map(str => str.trim());
-    if (!trimmedArray.includes(tagInputValue.trim())) {
+    const trimmedArray = tagsListValues.map(str => str.replace('\n','').trim());
+    if (!trimmedArray.includes(tagInputValue)) {
         tagsListValues.push(tagInputValue)
 
         const filteredArr = tagsListValues.filter(function (element) {
-            return element !== '';
+            return element.trim() !== '';
         });
 
         tagsList.html(filteredArr.join(', '))
@@ -16,23 +18,27 @@ function pushNewTagToTable(tagInputValue) {
 
 }
 
-$('#add_tag_button').click(function () {
 
-    const tagValue = $('#tagValue').val()
-    if (tagValue.trim() === '') {
-        alert('Tag input can not be empty')
-    } else {
-        $.ajax({
-            type: 'POST',
-            url: $(this).attr('action'),
-            data: {
-                tag_name: tagValue,
-                csrfmiddlewaretoken: getCookie('csrftoken')
-            },
-            success: function (response) {
-                $('#tagValue').val('');
-                pushNewTagToTable(tagValue)
-            }
-        });
-    }
-})
+$(document).ready(
+    $('#add_tag_button').click(function () {
+
+        const tagValue = $('#tagValue').val()
+        if (tagValue.trim() === '') {
+            alert('Tag input can not be empty')
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: $(this).attr('action'),
+                data: {
+                    tag_name: tagValue,
+                    csrfmiddlewaretoken: getCookie('csrftoken')
+                },
+                success: function (response) {
+                    $('#tagValue').val('');
+                    pushNewTagToTable(tagValue)
+                }
+            });
+        }
+    })
+)
+
