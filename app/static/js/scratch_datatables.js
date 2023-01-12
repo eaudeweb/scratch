36b66dfaa8adc7d0179ba05ff206d800b0cdf9e1
Ariel Pontes <pontes.ariel@gmail.com>
2023-01-12 10:51:53 +0200

    Fix awards/archive pages

--- app/static/js/scratch_datatables.js
+++ app/static/js/scratch_datatables.js
@@ -15,9 +15,9 @@ function getCookie(name) {
 }
 
 $.urlParam = function (doc, name) {
-    const results = new RegExp('[\?&]' + name + '=([^&#]*)')
-                      .exec(doc.location.search);
-    return (results !== null) ? decodeURIComponent(results[1]) : '';
+  const results = new RegExp('[\?&]' + name + '=([^&#]*)')
+    .exec(doc.location.search);
+  return (results !== null) ? decodeURIComponent(results[1]) : '';
 };
 
 function toggleFavourite(x) {
@@ -48,10 +48,10 @@ function toggleSeen(x) {
     try {
       usr.textContent = document.getElementById("usr_seen_by").textContent;
     }
-    catch(err){
+    catch (err) {
 
     }
-      value = true;
+    value = true;
   }
 
   return value;
@@ -67,7 +67,7 @@ function buttonsClick() {
     $.ajax({
       type: "POST",
       url: $(this).attr('data-url'),
-      data: {favourite: value},
+      data: { favourite: value },
       success: function () {
         console.log('Success');
       },
@@ -84,7 +84,7 @@ function buttonsClick() {
     $.ajax({
       type: "POST",
       url: $(this).attr('data-url'),
-      data: {seen: value},
+      data: { seen: value },
       success: function () {
         console.log('Success');
       },
@@ -157,26 +157,48 @@ const tableColumns = {
   ],
 };
 
-function initDataTables(){
-  $.fn.dataTable.moment( 'DD MMM YYYY');
-  $.fn.dataTable.moment( 'DD MMM YYYY, HH:mm');
-  
+function tenderData(d) {
+  d.organization = $('#id_organization').val();
+  d.status = $('#id_status').val();
+  d.source = $('#id_source').val();
+  d.favourite = $('#id_favourite').val();
+  d.has_keywords = $('#id_keyword').val();
+  d.notice_type = $('#id_type').val();
+  d.seen = $('#id_seen').val();
+  d.tags = $('#id_tags').val().join(',');
+  d.ungm_deadline_today = ungm_deadline_today;
+  d.ted_published_today = ted_published_today;
+  d.ted_deadline_today = ted_deadline_today;
+  d.ungm_published_today = ungm_published_today;
+}
+
+function awardData(d) {
+  d.organization = $('#id_organization').val();
+  d.source = $('#id_source').val();
+  d.vendor = $('#id_vendor').val();
+  d.value = $('#id_value').val();
+}
+
+function initDataTables() {
+  $.fn.dataTable.moment('DD MMM YYYY');
+  $.fn.dataTable.moment('DD MMM YYYY, HH:mm');
+
   let doc = this;
-  
+
   const searchTerm = $.urlParam(doc, 'terms');
   const ungm_deadline_today = $.urlParam(doc, 'ungm_deadline_today');
   const ted_published_today = $.urlParam(doc, 'ted_published_today');
   const ted_deadline_today = $.urlParam(doc, 'ted_deadline_today');
   const ungm_published_today = $.urlParam(doc, 'ungm_published_today');
 
-  const baseTableOptions = {
+  const tenderOptions = {
     "columnDefs": [
       { "width": "55%", "targets": 0 },
       { "width": "10%", "targets": 3 },
       { "width": "8%", "targets": 4 },
       { "width": "8%", "targets": 5 },
     ],
-    "order": [[ 4, "desc" ]],
+    "order": [[4, "desc"]],
     "pageLength": 10,
     "lengthChange": false,
     "search": {
@@ -206,63 +228,71 @@ function initDataTables(){
       {
         "targets": 0,
         "orderable": true,
-          "render": function ( data, type, row ) {
-              return '<a href="' + row['url'] +'">' + data + '</a>';
-          }
+        "render": function (data, type, row) {
+          return '<a href="' + row['url'] + '">' + data + '</a>';
+        }
       },
     ],
-    "drawCallback": function(settings) {
-        $("i").click(buttonsClick);
-        activateHover();
+    "columns": [
+      { "data": "title" },
+      { "data": "source" },
+      { "data": "organization" },
+      { "data": "deadline" },
+      { "data": "published" },
+      { "data": "tags" },
+      { "data": "notice_type" }
+    ],
+    "drawCallback": function (settings) {
+      $("i").click(buttonsClick);
+      activateHover();
     }
-  }
+  };
+
+  const archiveOptions = {
+    ...tenderOptions,
+    "ajax": {
+      ...tenderOptions.ajax,
+      "url": "/archive/ajax"
+    }
+  };
+
+  const awardOptions = {
+    ...tenderOptions,
+    "ajax": {
+      "url": "/awards/ajax",
+      "type": "GET",
+      "data": function (d) {
+        d.organization = $('#id_organization').val();
+        d.source = $('#id_source').val();
+        d.vendor = $('#id_vendor').val();
+        d.value = $('#id_value').val();
+      }
+    },
+    "columns": [
+      { "data": "title" },
+      { "data": "source" },
+      { "data": "organization" },
+      { "data": "award_date" },
+      { "data": "renewal_date" },
+      { "data": "vendor" },
+      { "data": "value" },
+      { "data": "currency" }
+    ]
+  };
 
   const tableOptions = {
-    "tenders_table": {
-      ...baseTableOptions,
-      "columns": [
-        { "data": "title" },
-        { "data": "source" },
-        { "data": "organization" },
-        { "data": "deadline" },
-        { "data": "published" },
-        { "data": "tags" },
-        { "data": "notice_type" }
-      ]
-    },
-    "archive_table": {
-      ...baseTableOptions,
-      "columns": [
-        { "data": "title" },
-        { "data": "source" },
-        { "data": "organization" },
-        { "data": "deadline" },
-        { "data": "published" },
-        { "data": "notice_type" }
-      ]
-    },
-    "contract_awards_table": {
-      ...baseTableOptions,
-      "columns": [
-        { "data": "title" },
-        { "data": "source" },
-        { "data": "organization" },
-        { "data": "award_date" },
-        { "data": "renewal_date" },
-        { "data": "vendor" },
-        { "data": "value" },
-        { "data": "currency" }
-      ]
-    },
+    "tenders_table": tenderOptions,
+    "archive_table": archiveOptions,
+    "contract_awards_table": awardOptions,
     "tender_detail_table": {
       "searching": false,
       "bPaginate": false,
       "bLengthChange": false,
-      "bInfo" : false,
+      "bInfo": false,
       "ordering": false,
-      "drawCallback": function(settings) {
-          $('i').click(buttonsClick);
-          activateHover();
+      "drawCallback": function (settings) {
+        $('i').click(buttonsClick);
+        activateHover();
       }
     }
   }
@@ -271,17 +301,17 @@ function initDataTables(){
   return table.DataTable(tableOptions[table.attr("id")]);
 }
 
-function activateHover(){
-  $(".li_buttons").mouseenter(function(){
+function activateHover() {
+  $(".li_buttons").mouseenter(function () {
     let icon = $(this).find("i");
-    if(icon.hasClass("pressed")){
+    if (icon.hasClass("pressed")) {
       icon.addClass("fa-regular").removeClass("fa-solid");
     } else {
       icon.addClass("fa-solid").removeClass("fa-regular");
     }
-  }).mouseleave(function(){
+  }).mouseleave(function () {
     let icon = $(this).find("i");
-    if(icon.hasClass("pressed")){
+    if (icon.hasClass("pressed")) {
       icon.addClass("fa-solid").removeClass("fa-regular");
     } else {
       icon.addClass("fa-regular").removeClass("fa-solid");
@@ -289,7 +319,7 @@ function activateHover(){
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('#id_vendor').select2();
   $('#id_organization').select2();
   $('#id_tags').select2();
